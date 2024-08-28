6aca60de3e0eb6206e4ce0a75f0c3146d509685c
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2024-08-28 21:59:16 +0200

    correct com selection animation, colored counter, js coded responsiveness for certain viewports

--- src/script.js
+++ src/script.js
@@ -69,9 +69,9 @@ function animateBattle(comInput) {
         setTimeout(() => desaturatePaper(), 375)
         setTimeout(() => saturateScissors(), 250)
         setTimeout(() => desaturateScissors(), 500)
-        counter < 5
-            ? setTimeout(() => startAnimation(counter + 1), 500)
-            : setTimeout(showResult, 500)
+        if (counter < 5)
+            return setTimeout(() => startAnimation(counter + 1), 500)
+        setTimeout(showResult, 500)
     }
 
     startAnimation(0)
@@ -80,6 +80,9 @@ function animateBattle(comInput) {
 function calculateResult(ownInput) {
     const ownScore = parseInt(document.getElementById('ownScore').innerHTML)
     const comScore = parseInt(document.getElementById('comScore').innerHTML)
+
+    const ownPoints = document.getElementById('ownScore')
+    const comPoints = document.getElementById('comScore')
 
     const winnerText = document.getElementById('winnerText')
 
@@ -121,6 +124,16 @@ function calculateResult(ownInput) {
 
     animateBattle(comInput)
 
+    function animateWin() {
+        ownPoints.style.color = '#357a38'
+        setTimeout(() => (ownPoints.style.color = '#fffafa'), 1000)
+    }
+
+    function animateLoss() {
+        comPoints.style.color = '#f13535'
+        setTimeout(() => (comPoints.style.color = '#fffafa'), 1000)
+    }
+
     if (comInput === 'rock') {
         switch (ownInput) {
             case 'rock':
@@ -134,6 +147,7 @@ function calculateResult(ownInput) {
                     3000
                 )
                 setTimeout(() => (winnerText.innerHTML = 'You Win!'), 3000)
+                setTimeout(() => animateWin(), 3000)
                 break
             case 'scissors':
                 setTimeout(
@@ -143,6 +157,7 @@ function calculateResult(ownInput) {
                     3000
                 )
                 setTimeout(() => (winnerText.innerHTML = 'COM Wins!'), 3000)
+                setTimeout(() => animateLoss(), 3000)
                 break
         }
     } else if (comInput === 'paper') {
@@ -155,6 +170,7 @@ function calculateResult(ownInput) {
                     3000
                 )
                 setTimeout(() => (winnerText.innerHTML = 'COM Wins!'), 3000)
+                setTimeout(() => animateLoss(), 3000)
                 break
             case 'paper':
                 setTimeout(() => (winnerText.innerHTML = 'TIE!'), 3000)
@@ -167,6 +183,7 @@ function calculateResult(ownInput) {
                     3000
                 )
                 setTimeout(() => (winnerText.innerHTML = 'You Win!'), 3000)
+                setTimeout(() => animateWin(), 3000)
                 break
         }
     } else if (comInput === 'scissors') {
@@ -179,6 +196,7 @@ function calculateResult(ownInput) {
                     3000
                 )
                 setTimeout(() => (winnerText.innerHTML = 'You Win!'), 3000)
+                setTimeout(() => animateWin(), 3000)
                 break
             case 'paper':
                 setTimeout(
@@ -188,6 +206,7 @@ function calculateResult(ownInput) {
                     3000
                 )
                 setTimeout(() => (winnerText.innerHTML = 'COM Wins!'), 3000)
+                setTimeout(() => animateLoss(), 3000)
                 break
             case 'scissors':
                 setTimeout(() => (winnerText.innerHTML = 'TIE!'), 3000)
@@ -204,4 +223,43 @@ function calculateResult(ownInput) {
     setTimeout(() => (scissorsButton.style.filter = 'saturate(100%)'), 4000)
 }
 
+function responsiveViewport() {
+    const comSelection = document.getElementById('comSelection')
+    const footer = document.getElementById('footer')
+    const heading = document.getElementById('heading')
+    const main = document.getElementById('main')
+    const winnerText = document.getElementById('winnerText')
+
+    if (window.innerWidth < 768 || window.innerHeight < 768) {
+        if (
+            window.innerWidth > window.innerHeight ||
+            (window.innerHeight < 500 && window.innerWidth < 400)
+        ) {
+            footer.style.display = 'none'
+            heading.style.display = 'none'
+            main.style.height = '100svh'
+            if (
+                window.innerHeight < 384 ||
+                (window.innerHeight < 500 && window.innerWidth < 400)
+            ) {
+                winnerText.style.display = 'none'
+            } else {
+                winnerText.style.display = 'block'
+            }
+            if (window.innerWidth >= 1024) {
+                comSelection.style.top = '15%'
+            } else {
+                comSelection.style.top = '25%'
+            }
+        } else {
+            footer.style.display = 'flex'
+            heading.style.display = 'block'
+            main.style.height = 'calc(100svh - 64px)'
+        }
+    }
+}
+
+responsiveViewport()
+
 document.addEventListener('keydown', focusTrap)
+window.addEventListener('resize', responsiveViewport)
